27b63f38f965ba74b72ade9ad015575b53ae1d20
akshayshetty709 <114096841+akshayshetty709@users.noreply.github.com>
2022-12-26 23:31:23 +0530

    Create behave.sh

---
+++ behave.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "installing behave"
+python_install()
+{
+    sudo apt install python3 -y
+    if [ $?-ne 0 ]
+    then
+         echo "python install failed"
+    else
+         echo "python installed succesfully"
+    fi
+}
+behave_install()
+{
+    sudo apt install python3-behave -y
+    if [ $?-ne 0 ]
+    then
+         echo "behave install failed"
+    else
+         echo "behave installed succesfully"
+    fi
+}
+cleanup_workspace()
+{
+  echo "removing old behave files"
+  sudo rm -r /var/lib/jenkins/workspace/testbehave/features
+  if [ $?-ne 0 ]
+  then
+        echo "error while removing behave files"
+  else
+        echo "behave files removed sucessfully"
+  fi
+  }
+ behave_steps()
+ {
+   echo "steps torun BDD"
+   sudo mkdir -p /var/lib/jenkins/workspace/testbehave/features/steps
+   sudo cp-r ./test.feature /var/lib/jenkins/workspace/testbehave/features
+   sudo cp-r ./test.py /var/lib/jenkins/workspace/testbehave/features/steps
+   behave
+   }
+   python_install
+   behave_install
+   cleanup_workspace
+   behave-steps 
