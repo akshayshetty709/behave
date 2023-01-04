74a410d872ee421c4192c9188bbbc32428fbf59f
akshayshetty709 <114096841+akshayshetty709@users.noreply.github.com>
2023-01-04 16:24:53 +0530

    Delete comaprev1.sh

--- comaprev1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-old_version=$1
-new_version=$2
-update_version()
-{
-version=$(echo "$@" | tr [:space:] "\n" | sort -rV | head -1)
-echo "newer version is ${version}"
-}
-update_version $1 $2
